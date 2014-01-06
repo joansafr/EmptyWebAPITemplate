47065298595ab9251146a8059d70742355a3c86f
José Antonio Salinas Francia <joansafr@gmail.com>
2014-01-06 02:24:04 -0500

    Initial Commit

---
+++ net40WebAPITemplate/net40WebAPITemplate/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace net40WebAPITemplate
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+        }
+		
+        ////Descomenta esto para habilitar el Cross Domain
+        //protected void Application_BeginRequest(object sender, EventArgs e)
+        //{
+        //    HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+        //    if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+        //    {
+        //        HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+        //        HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+        //        HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+        //        HttpContext.Current.Response.End();
+        //    }
+        //}
+    }
+}
---
+++ net45WebAPITemplate/net45WebAPITemplate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="net45WebAPITemplate.WebApiApplication" Language="C#" %>
